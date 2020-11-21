fb909bae691eb0c31c90a33febace6cdf03d03ba
ksarvani2000 <k.sarvani2000@gmail.com>
2020-11-21 09:45:38 +0530

    updated

---
+++ BloodProject/app/src/main/java/com/example/bloodproject/MyModel.java
@@ -0,0 +1,67 @@
+package com.example.bloodproject;
+
+public class MyModel {
+    String fname,lname,mail,mobile,age,date,gender,bloodgroup,image,address,rating;
+
+    public String getFname() {
+        return fname;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public String getBloodgroup() {
+        return bloodgroup;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public MyModel() {
+    }
+
+    public MyModel(String fname, String lname, String mail, String mobile, String age, String date, String gender, String bloodgroup, String image, String address, String rating) {
+        this.fname = fname;
+        this.lname = lname;
+        this.mail = mail;
+        this.mobile = mobile;
+        this.age = age;
+        this.date = date;
+        this.gender = gender;
+        this.bloodgroup = bloodgroup;
+        this.image = image;
+        this.address = address;
+        this.rating = rating;
+    }
+
+}
