82e9ad4cd762779caaee6297ae5802377b6439e9
ksarvani2000 <k.sarvani2000@gmail.com>
2020-11-22 15:28:36 +0530

    original

---
+++ Rakthadaan/app/src/main/java/com/example/rakthadaan/HomeActivity.java
@@ -0,0 +1,24 @@
+package com.example.rakthadaan;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+
+    public void receiver(View view) {
+        startActivity(new Intent(com.example.rakthadaan.HomeActivity.this, LoginActivity.class));
+    }
+
+    public void donar(View view) {
+        startActivity(new Intent(com.example.rakthadaan.HomeActivity.this,LoginActivity.class));
+    }
+}
