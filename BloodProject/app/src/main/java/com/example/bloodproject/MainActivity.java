7493c9ccd6fa8dffbe2d6842a024b911abaed959
Android2k2k <project18b0@gmail.com>
2020-12-03 17:58:43 +0530

    Anim

--- BloodProject/app/src/main/java/com/example/bloodproject/MainActivity.java
+++ BloodProject/app/src/main/java/com/example/bloodproject/MainActivity.java
@@ -2,12 +2,30 @@ package com.example.bloodproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.ObjectAnimator;
+import android.animation.PropertyValuesHolder;
+import android.animation.ValueAnimator;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView ivTop,ivLogo,ivBottom;
+    TextView textView;
+    CharSequence charSequence;
+    int index;
+    long delay = 2000;
+    Handler handler = new Handler();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,10 +34,62 @@ public class MainActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(MainActivity.this, HomeActivity.class));
+                startActivity(new Intent(MainActivity.this, HomeActivity.class)
+                        .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+                finish();
             }
-        }, 3000);
+        }, 4000);
+        ivTop = findViewById(R.id.iv_top);
+        ivLogo = findViewById(R.id.iv_logo);
+        ivBottom= findViewById(R.id.iv_bottom);
+        textView = findViewById(R.id.text_view);
+
+        //set fullscreen
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN
+                ,WindowManager.LayoutParams.FLAG_FULLSCREEN)  ;
+        //initialize top animation
+        Animation animation1 = AnimationUtils.loadAnimation(this
+                ,R.anim.top_wave);
+        //start top animation
+        ivTop.setAnimation(animation1);
+        //initialize object animator
+        ObjectAnimator objectAnimator = ObjectAnimator.ofPropertyValuesHolder(
+                ivLogo,
+                PropertyValuesHolder.ofFloat("scaleX",1.2f),
+                PropertyValuesHolder.ofFloat("scaleY",1.2f)
+        );
+        //set duration
+        objectAnimator.setDuration(500);
+        //set repeat count
+        objectAnimator.setRepeatCount(ValueAnimator.INFINITE);
+        //set repeat mode
+        objectAnimator.setRepeatCount(ValueAnimator.REVERSE);
+        //start animator
+        objectAnimator.start();
+
+        animateText("Rakthadaan");
+
+        Animation animation2 = AnimationUtils.loadAnimation(this,
+                R.anim.bottom_wave);
+        ivBottom.setAnimation(animation2);
 
 
+    }
+
+    Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            textView.setText(charSequence.subSequence(0,index++));
+            if(index<=charSequence.length()){
+                handler.postDelayed(runnable,delay);
+            }
+        }
+    };
+    public void animateText(CharSequence cs){
+        charSequence = cs;
+        index = 0;
+        textView.setText("Rakthadaan");
+        handler.removeCallbacks(runnable);
+        handler.postDelayed(runnable,delay);
     }
 }
